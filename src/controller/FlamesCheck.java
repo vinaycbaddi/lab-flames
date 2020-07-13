2d7482941672745d6cb209a03df9789401ae8761
root <root@LAPTOP-ODE2T0U9.localdomain>
2020-07-14 02:12:03 +0530

    Vinay(S-55)

--- src/controller/FlamesCheck.java
+++ src/controller/FlamesCheck.java
@@ -6,6 +6,7 @@ package controller;
 */
 import java.io.IOException;
 
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.FlamesCheckService;
 
 
 @WebServlet("/flames")
@@ -33,35 +35,35 @@ public class FlamesCheck extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		/*
-		 * Uncomment the below code to test your output 
-		 * String name1 = request.getParameter("your"); 
-		 * String name2 = request.getParameter("crush");
-		 * 
-		 * FlamesCheckService fcs = new FlamesCheckService();
-		 * 
-		 * char k = fcs.findFlames(name1,name2);
-		 * 
-		 * if (k == 'f') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/friends.jsp"
-		 * ); rd.forward(request, response);
-		 * 
-		 * } else if (k == 'l') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/lovers.jsp")
-		 * ; rd.forward(request, response);
-		 * 
-		 * } else if (k == 'a') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher(
-		 * "/WEB-INF/views/affection.jsp"); rd.forward(request, response);
-		 * 
-		 * } else if (k == 'm') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher(
-		 * "/WEB-INF/views/marriage.jsp"); rd.forward(request, response);
-		 * 
-		 * } else if (k == 'e') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/enemies.jsp"
-		 * ); rd.forward(request, response);
-		 * 
-		 * } else if (k == 's') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher(
-		 * "/WEB-INF/views/siblings.jsp"); rd.forward(request, response);
-		 * 
-		 * }
-		 */
+	
+		  //Uncomment the below code to test your output 
+		  String name1 = request.getParameter("your"); 
+		  String name2 = request.getParameter("crush");
+		  
+		  FlamesCheckService fcs = new FlamesCheckService();
+		  
+		  char k = fcs.findFlames(name1,name2);
+		  
+		  if (k == 'f') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/friends.jsp"
+		  ); rd.forward(request, response);
+		  
+		  } else if (k == 'l') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/lovers.jsp")
+		  ; rd.forward(request, response);
+		  
+		  } else if (k == 'a') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher(
+		  "/WEB-INF/views/affection.jsp"); rd.forward(request, response);
+		  
+		  } else if (k == 'm') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher(
+		  "/WEB-INF/views/marriage.jsp"); rd.forward(request, response);
+		  
+		  } else if (k == 'e') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/enemies.jsp"
+		  ); rd.forward(request, response);
+		  
+		  } else if (k == 's') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher(
+		  "/WEB-INF/views/siblings.jsp"); rd.forward(request, response);
+		  
+		  }
+		 
 	}
 
 }
--- src/service/FlamesCheckService.java
+++ src/service/FlamesCheckService.java
@@ -1,9 +1,53 @@
 package service;
-
 // Create a class called FlamesCheckService
 // FlamesCheckService has a method findFlames to find the flames between two names
 // char findFlames(String name1, String name2) takes two strings as arguments
 // Your task is to calculate the flames value and return the corresponding character as output 
 // You must return only the following values ['f','l','a','m','e','s']
 // change the return value at the end of the method corresponding to your return value
+public class FlamesCheckService {
+    public char findFlames(String name1, String name2) {
+        StringBuilder str1 = new StringBuilder(name1);
+        StringBuilder str2 = new StringBuilder(name2);
+        String str="";
+        int a=str1.length();
+        int b=str2.length();
+        for(int i=0;i<a;i++){
+            for (int j=0;j<b;j++){
+                if(str1.charAt(i)==str2.charAt(j)){
+                 str1.setCharAt(i,'0');
+                 str2.setCharAt(j,'0');
+                }
+            }
+        }
+        int count=0;
+        str=str1.append(str2).toString();
+        for (int i=0;i<str.length();i++){
+            if(str.charAt(i)!='0'){
+                count++;
+            }
+        }
 
+        String flames="flames";
+		StringBuilder sb3=new StringBuilder(flames);
+		char flameResult=0;
+		
+		while(sb3.length()!=1) {
+			int y1=count%sb3.length();
+			String temp;
+			
+			if(y1!=0) {
+				temp=sb3.substring(y1)+sb3.substring(0, y1-1);
+			}
+			else {
+				temp =sb3.substring(0, sb3.length()-1);
+			}
+			sb3=new StringBuilder(temp);
+			flameResult=sb3.charAt(0);
+			
+		}
+		
+		System.out.print(flameResult);
+		return flameResult;
+    }
+}
